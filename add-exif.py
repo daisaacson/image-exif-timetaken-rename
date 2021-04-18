7f67329395ef900a2ff5547b5fb46fd30975869f
daisaacson <49328302+daisaacson@users.noreply.github.com>
2021-04-18 09:34:46 -0500

    add exif 306 to images

---
+++ add-exif.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys, time
+from PIL import Image
+import piexif
+
+fn = sys.argv[1]
+taken = sys.argv[2]
+
+print ("Filename: %s"%(fn))
+img = Image.open(fn)
+try:
+    exif_dict = piexif.load(img.info["exif"])
+    exif_dict["0th"][306] = bytes(taken,"UTF-8")
+    exif_bytes = piexif.dump(exif_dict)
+    img.save("new_" + fn, "jpeg", exif=exif_bytes)
+except KeyError:
+    exif_dict = {"0th":{}}
+    w, h = img.size
+    exif_dict["0th"][piexif.ImageIFD.XResolution] = (w, 1)
+    exif_dict["0th"][piexif.ImageIFD.YResolution] = (h, 1)
+    exif_dict["0th"][306] = bytes(taken,"UTF-8")
+    exif_bytes = piexif.dump(exif_dict)
+    img.save("new_" + fn, "jpeg", exif=exif_bytes)
+print ("exif: %s"%(exif_dict))
