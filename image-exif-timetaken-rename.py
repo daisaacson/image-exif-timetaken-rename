8ccc484c9104a455e6e919d40d94b28bfb3d5af0
daisaacson <49328302+daisaacson@users.noreply.github.com>
2021-05-11 18:45:12 -0500

    added video processing

--- image-exif-timetaken-rename.py
+++ image-exif-timetaken-rename.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-import os, sys, time, datetime
+import os, sys, time, datetime, pathlib
 from dateutil.relativedelta import relativedelta
 from optparse import OptionParser, OptionGroup
+# Image files
 from PIL import Image
-import pathlib
+# HEIC files from Apple Phones 
 import piexif
 import pyheif
+# Video files
+import ffmpeg
 
 __version__ = '0.1'
 
@@ -14,17 +17,30 @@ __version__ = '0.1'
 def GetImageDate(fn):
     try:
         if pathlib.Path(fn).suffix.lower() == '.heic':
-            if options.verbose: print("Intput file is HEIC: %s" % fn) 
+            if options.verbose: print("Input file is HEIC: %s" % fn) 
             heif = pyheif.read_heif(fn)
             for m in heif.metadata or []:
                 if m['type'] == 'Exif':
                     heif_meta = piexif.load(m['data'])
             return time.strptime(heif_meta['0th'][306].decode('utf-8'),"%Y:%m:%d %H:%M:%S")
+        if pathlib.Path(fn).suffix.lower() == '.mov' or pathlib.Path(fn).suffix.lower() == '.mp4':
+            if options.verbose: print("Input file is vidio: %s" % fn)
+            probe = ffmpeg.probe(fn)
+            video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+            creation_time = video_stream['tags']['creation_time'] + "GMT"
+            if options.debug:
+                print("Creation Time %s" % creation_time)
+                g = datetime.datetime.fromtimestamp(time.mktime(time.strptime(creation_time,"%Y-%m-%dT%H:%M:%S.000000Z%Z")),tz=datetime.timezone.utc)
+                #g = time.strptime(creation_time,"%Y-%m-%dT%H:%M:%S.000000Z%Z")
+                c = time.localtime(time.mktime(time.strptime(creation_time,"%Y-%m-%dT%H:%M:%S.000000Z%Z")))
+                print("GMT Time %s" % str(g))
+                print("CDT Time %s" % str(c))
+            return g
         return time.strptime(Image.open(fn)._getexif()[306],"%Y:%m:%d %H:%M:%S")
     except IOError:
         print ("File %s not found" % fn)
-    except TypeError:
-        print ("File %s exif data not found" % fn)
+    #except TypeError:
+    #    print ("File %s exif data not found" % fn)
 #%%
 def GetCompare(filedate, comparedate):
     d = datetime.datetime.strptime(filedate,"%Y%m%d-%H%M%S").date()
