438a78fc0567c8a20065ea4c0a83e348f0969975
daisaacson <49328302+daisaacson@users.noreply.github.com>
2024-03-30 15:12:00 -0500

    improvments

--- Convert.ps1
+++ Convert.ps1
@@ -1,50 +1,47 @@
 # New Parent Path
 $newParentPath = "E:\Family"
+$ffmpeg = "~\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg.Shared_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-6.1.1-full_build-shared\bin\ffmpeg.exe"
 
-# Lets compress the Jpegs
-Get-ChildItem -Recurse -Include "*.jpg", "*.jpeg", "*.png" -Exclude "*Day Care*" * | Foreach-Object {
-	$fileHash = (Get-FileHash -Algorithm SHA256 $_).Hash.ToLower()
-	$newFileName = "$newParentPath\$($fileHash.Substring(0,2))\$($fileHash.Substring(0,8))_$($_.basename).jpg"
+$files = Get-ChildItem -Recurse -File * 
+$i = 0
+
+foreach ($file in $files) {
+	$fileHash = (Get-FileHash -Algorithm SHA256 $file).Hash.ToLower()
+	$folder = "$($fileHash.Substring(0,2))"
+	$prefix = "$($fileHash.Substring(0,8))_"
+	$newFileName = "$newParentPath\$($folder)\$($prefix)$($file.basename).jpg"
+
+	[int]$percentComplete = (($i+1)/$files.count)*100
+
+	Write-Progress -Activity "Converting" -Status "$percentComplete%: $($file.basename)" -Id 0 -percentComplete $percentComplete
+
 	if ( -Not (Test-Path $newFileName) ) {
-		Write-Host -Foreground Green "Converting: $($_.name)"
 		If ( -Not (Test-Path -PathType Container $(Split-Path $newFileName)) ) {
-			New-Item -ItemType Directory -Path $(Split-Path $newFileName) -Verbose
+			[void](New-Item -ItemType Directory -Path $(Split-Path $newFileName))
 		}
-		& ffmpeg.exe -i "$($_.fullname)" -codec:v mjpeg -qscale:v 3 "$newFileName"
-	} else {
-		Write-Host -Foreground Yellow "Skipping: $($_.name)"
-	}
-}
-
-## Convert iPhone heic images to jpeg.  This currenlty fails and is in place in case it ever works in the future
-## See commented out LINUX bash script below
-#Get-ChildItem -Recurse -Include "*.heic" * | Foreach-Object {
-#	$fileHash = (Get-FileHash -Algorithm SHA256 $_).Hash.ToLower()
-#	$newFileName = "$newParentPath\$($fileHash.Substring(0,2))\$($fileHash.Substring(0,8))_$($_.basename).jpg"
-#	if ( -Not (Test-Path $newFileName) ) {
-#		Write-Host -Foreground Green "Converting: $($_.name)"
-#		If ( -Not (Test-Path -PathType Container $(Split-Path $newFileName)) ) {
-#			New-Item -ItemType Directory -Path $(Split-Path $newFileName) -Verbose
-#		}
-#		& ffmpeg.exe -i "$($_.fullname)" -codec:v mjpeg -movflags -qscale:v 3 "$($newFileName).jpg"
-#	} else {
-#		Write-Host -Foreground Yellow "Skipping: $($_.name)"
-#	}
-#}
-
-# Copy Day Care photes, they are small, plus running them through ffmpeg causes the Aluratek Picture Frame to not show red colors
-Get-ChildItem -Recurse -Include "*Day Care*" * | Foreach-Object {
-	$fileHash = (Get-FileHash -Algorithm SHA256 $_).Hash.ToLower()
-	$newFileName = "$newParentPath\$($fileHash.Substring(0,2))\$($fileHash.Substring(0,8))_$($_.name)"
-	if ( -Not (Test-Path $newFileName) ) {
-		Write-Host -Foreground Green "Copying: $($_.name)"
-		If ( -Not (Test-Path -PathType Container $(Split-Path $newFileName)) ) {
-			New-Item -ItemType Directory -Path $(Split-Path $newFileName) -Verbose
+		
+		switch -regex ($file) {
+			".*Day Care.*" {
+				# Copy Day Care photes, they are small, plus running them through ffmpeg causes the Aluratek Picture Frame to not show red colors
+				[void](Copy-Item -Destination $newFileName $file.fullname)
+				break
+			}
+			"\.jp.?g$" {
+				# Lets compress the Jpegs
+				& $ffmpeg -hide_banner -loglevel warning -i "$($file.fullname)" -update true -codec:v mjpeg -qscale:v 3 "$newFileName"
+				break
+			}
+			"\.heic$" {
+				# Convert iPhone heic images to jpeg.  This currenlty fails and is in place in case it ever works in the future
+				& $ffmpeg -hide_banner -loglevel warning -i "$($file.fullname)" -update true -codec:v mjpeg -movflags -qscale:v 3 "$($newFileName).jpg"
+				break
+			}
 		}
-		Copy-Item -Destination $newFileName $_.fullname
 	} else {
-		Write-Host -Foreground Yellow "Skipping: $($_.name)"
+		Write-Progress -Activity "Converting" -Status "$percentComplete%: Skipping $($file.basename)" -Id 0 -percentComplete $percentComplete
 	}
+
+	$i++
 }
 
 ## LINUX
